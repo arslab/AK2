153bd8f77102d8c5972e888358a248a832d0c851
woodygaragelab <woodygaragelab@gmail.com>
2022-03-08 11:09:57 +0900

    change footer

--- src/AppFooter.js
+++ src/AppFooter.js
@@ -6,9 +6,9 @@ import { AmplifySignOut } from '@aws-amplify/ui-react';
 function AppFooter() {
   return (
     <footer className="fixed-bottom AppFooter">
-      <div>食品</div>
-      <div>生活用品</div>
-      <div>おすすめ</div>
+      <div>AAA</div>
+      <div>BBB</div>
+      <div>CCC</div>
       <AmplifySignOut  buttonText="Sign Out" className="col-2"/>
     </footer>
   );
