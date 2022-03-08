09423e2edcd1daf39fe167530c9151fcea990442
woodygaragelab <woodygaragelab@gmail.com>
2022-03-08 12:40:30 +0900

    remove comment

--- src/Kintai.js
+++ src/Kintai.js
@@ -12,11 +12,6 @@ import React, { useState, useEffect } from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Button from 'react-bootstrap/Button';
 import Card from 'react-bootstrap/Card';
-// import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-// import { faTrash } from "@fortawesome/free-solid-svg-icons";
-
-// const initialFormState = {Date: '', userID: '', Time:'',  Remarks:'',  
-//                           TrxCode: '' }
 
 function Kintai() {
   const [items, setItems] = useState([]);
@@ -50,7 +45,7 @@ function Kintai() {
           items.map(item => (
             <Card key={item.id}>
             <Card.Body className="AppBg1">
-                  <div>{item.Date}</div>                      {/* アイテム名 */}
+                  <div>{item.Date}</div>         
             </Card.Body>
             </Card>
           ))
