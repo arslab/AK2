d40f2818479be91b1aed5469ffb76c2cdf94d93d
woodygaragelab <woodygaragelab@gmail.com>
2022-03-08 11:01:48 +0900

    change to AK2

--- src/AppHeader.js
+++ src/AppHeader.js
@@ -5,12 +5,12 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 function AppHeader() {
   return (
       <header className="fixed-top AppHeader AppBg2">
-        <div className="col-4">Food Stock 0423 0839</div>
+        <div className="col-4">ARS Kintai2</div>
         <div className="col-6">
-          <div>在庫</div>
-          <div>賞味期限</div>
+          <div>XXXX</div>
+          <div>YYYY</div>
         </div>
-        <div className="col-2">再注文</div>
+        <div className="col-2">ZZZZ</div>
       </header>
   );
 }
