b6f3eda4cae15de05653c667367c41c667bdc618
woodygaragelab <woodygaragelab@gmail.com>
2022-03-15 14:04:17 +0900

    split

--- src/AK2.js
+++ src/AK2.js
@@ -4,8 +4,15 @@ import { Route, Switch} from 'react-router-dom';
 //import { Route, Routes} from 'react-router-dom';
 import 'bootstrap/dist/css/bootstrap.min.css'; // 標準スタイルは bootstrapを使う
 import './App.css';                            // アプリ共通スタイル（kzXxxxx）
-import SummaryPage   from './summarypage';
-import DetailPage   from './detailpage';
+import Taka1   from './Taka1';
+import Taka2   from './Taka2';
+import Kiji1   from './Kiji1';
+import Kiji2   from './Kiji2';
+import Naka1   from './Naka1';
+import Naka2   from './Naka2';
+import Ashi1   from './Ashi1';
+import Ashi2   from './Ashi2';
+import App     from './App';
 
   class AK2 extends React.Component {
 
@@ -17,9 +24,15 @@ import DetailPage   from './detailpage';
           <Router>   
           {/* <Routes>    */}
           <Switch>
-            <Route exact={true} path='/' component={DetailPage}/>
-            <Route exact={true} path='/summarypage'  component={SummaryPage}/>    
-            <Route exact={true} path='/detailpage'  component={DetailPage}/>    
+            <Route exact={true} path='/' component={App}/>
+            <Route exact={true} path='/taka1'  component={Taka1}/>    
+            <Route exact={true} path='/taka2'  component={Taka2}/>    
+            <Route exact={true} path='/ashi1'  component={Ashi1}/>    
+            <Route exact={true} path='/ashi2'  component={Ashi2}/>    
+            <Route exact={true} path='/kiji1'  component={Kiji1}/>    
+            <Route exact={true} path='/kiji2'  component={Kiji2}/>    
+            <Route exact={true} path='/naka1'  component={Naka1}/>    
+            <Route exact={true} path='/naka2'  component={Naka2}/>    
           </Switch>
           {/* </Routes> */}
           </Router>
---
+++ src/Kiji2.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import './App.css';
+import Box from '@mui/material/Box';
+import AppHeader from './AppHeader';
+import AppFooter from './AppFooter';
+import Kintai from './Kintai';
+//import { withAuthenticator } from '@aws-amplify/ui-react';
+//import 'bootstrap/dist/css/bootstrap.min.css';
+
+function App() {
+  // return (
+  //   <div className="mt-5 mb-5 container-fluid AppBg0">
+  //     <AppHeader/>
+  //     <Kintai/>
+  //     <AppFooter/>
+  //   </div>
+  // );
+  return (
+    <Box sx={{ display: 'flex', flexDirection: 'column', minHeight: '100vh'}}>
+      <AppHeader/>
+      <Box sx={{ py: 2, flex: 1, backgroundColor: (theme) => theme.palette.grey[100]}}>
+        <Kintai/>
+      </Box>
+      <AppFooter/>
+    </Box>
+  );
+}
+
+export default App;
