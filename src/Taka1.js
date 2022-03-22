130520f7bf424091c51b294c9524f9e242016ace
woodygaragelab <woodygaragelab@gmail.com>
2022-03-22 09:39:38 +0900

    removed legacy mui from taka1

--- src/Taka1.js
+++ src/Taka1.js
@@ -1,62 +1,24 @@
-import React from 'react'
-import { useState } from 'react';                           // state（コンポネント単位のデータ保存機能）
-import { useEffect } from 'react';                           // effect (state変化したときの処理機能)
+import React          from 'react'
+import { useState }   from 'react';                     // state（コンポネント単位のデータ保存機能）
+import { useEffect }  from 'react';                     // effect (state変化したときの処理機能)
 import { useHistory } from 'react-router-dom';
-import { makeStyles } from "@material-ui/core/styles";
-import {Card, CardHeader, CardContent, CardActions} from "@material-ui/core";
-import Grid from "@material-ui/core/Grid";
-import Typography from '@mui/material/Typography';
-
+import Card        from '@mui/material/Card';
+import CardHeader  from '@mui/material/CardHeader';
+import CardActions from '@mui/material/CardActions';
+import CardContent from '@mui/material/CardContent';
+import Button      from '@mui/material/Button';
+import Grid        from '@mui/material/Grid';
+import Typography  from '@mui/material/Typography';
 
 //import AppHeader from './AppHeader';
 import AppHeader from './TakaHeader';
 import AppFooter from './AppFooter';
 
-const useStyles = makeStyles((theme) => ({
-  typography: {
-    marginTop: theme.spacing(10),
-    marginBottom: theme.spacing(10),
-  },
-  grid: {
-    margin: "auto",
-  },
-  button: {
-    marginTop:    theme.spacing(5),
-    marginBottom: theme.spacing(5),
-    width: "250px",
-    height: "200px",
-    fontSize: "30px",
-    margin: theme.spacing(1),
-  },
-  card: {
-    marginTop:    theme.spacing(5),
-    marginBottom: theme.spacing(5),
-    width: "250px",
-    height: "200px",
-    margin: theme.spacing(1),
-  },
-  card_header_0: {
-    fontSize: "50px",
-    color:    "#FFFFFF",
-    backgroundColor: "#FF0044"
-  },
-  card_header_1: {
-    fontSize: "50px",
-    color:    "#FFFFFF",
-    backgroundColor: "#00EE00"
-  },
-  card_content: {
-    fontSize: "20px",
-    color:    "#444444",
-    backgroundColor: "#FFFFFF"
-  },
-}))
 const emps = [                                     // 社員出勤状況初期値
-  {'EmpNo':'11111', 'FirstName':'---','LastName':'データ取得中', 'status':'0' }
+  {'EmpNo':'00000', 'FirstName':'---','LastName':'データ取得中', 'status':'0' }
 ]
 
 function Taka1() {
-  const classes = useStyles();
   const history = useHistory();
   const [employees, setEmployees] = useState(emps);  
   const [datetime,  setDateTime]  = useState(new Date());  
@@ -64,7 +26,7 @@ function Taka1() {
     // 詳細画面へ遷移する。EmpNoを渡す。
     history.push({ pathname: '/taka2', state: { EmpNo: employees[index].EmpNo }});  
   }
-    
+  
   useEffect(() => {                            // 描画後の処理。タイマーでデータを定期更新する。
     fetchItems();                              // データを再取得
     const interval = setInterval(() => {
@@ -89,31 +51,52 @@ function Taka1() {
     .catch(error => console.log('error', error));
   }
 
+  // 勤怠を記録する（テスト用）
+  const createAttendance = (index) => {
+    sendAttendance(employees[index].CardID);  // APIをcallする。CardIDを渡す。
+  }
+  async function sendAttendance(CardID) {
+    var myHeaders = new Headers();
+    myHeaders.append("Content-Type", "application/json");
+    var raw = JSON.stringify({"CardID":CardID});
+    var requestOptions = {method: 'POST', headers: myHeaders, body: raw, redirect: 'follow' };
+    fetch("https://iv822lhlh5.execute-api.ap-northeast-1.amazonaws.com/dev/", requestOptions)
+    .then(response => response.text())
+    .then(async(response) => {
+      const apiData = JSON.parse(response);
+      console.log(apiData);
+    })
+    .catch(error => console.log('error', error));
+  }
+
   return (
     <div>
-      <AppHeader title="勤怠サマリー" datetime={datetime.toLocaleTimeString()}/>
+      <AppHeader title="勤怠状況サマリー" datetime={datetime.toLocaleTimeString()}/>
       <Grid container>
         {/* 社員の数だけGridを作成する */}
         {employees.map((emp, index) => (
-          <Grid item className={classes.grid} key={emp.EmpNo}>
-            <Card className={classes.card} onClick={() => selectEmp(index)} >
+          <Grid item sx={{}} key={emp.EmpNo}>
+            <Card sx={{width:"250px", height:"200px", margin:1}} >
               {/* statusによってCardHeaderの色を変える */}
               {emp.Status === '1' ?
-              <CardHeader title={emp.LastName} className={classes.card_header_1} /> :
-              <CardHeader title={emp.LastName} className={classes.card_header_0} />
+              <CardHeader title={emp.LastName} sx={{backgroundColor:"#00DD00",color:"#FFFFFF" }}/> :
+              <CardHeader title={emp.LastName} sx={{backgroundColor:"#FF0044",color:"#FFFFFF" }}/> 
               }
-              <CardContent className={classes.card_content}>
+              <CardContent sx={{backgroundColor:"#FFFFFF",color:"#444444",fontSize:"20px" }}>
                 {/* statusによって出勤/退勤を表示する */}
                 {emp.Status === '1' ?
-                <Typography variant="h5" component="div">出勤</Typography> :
-                <Typography variant="h5" component="div">退勤</Typography>
+                <Typography variant="h5" component="div" >出勤してます</Typography> :
+                <Typography variant="h5" component="div">退勤しました</Typography>
                 }
                 {/* 社員データを表示する */}
-                <Typography variant="h8" component="div" sx={{ color: 'text.secondary', fontSize: 16}}>
+                <Typography component="div" sx={{color:'text.secondary',fontSize:14, marginTop:1}}>
                   {emp.EmpNo} {emp.LastName} {emp.FirstName}
                 </Typography>
-
               </CardContent>
+              <CardActions>
+                <Button size="small" onClick={() => selectEmp(index)}>詳細</Button>
+                <Button size="small" onClick={() => createAttendance(index)}>IN/OUT(TEST)</Button>
+              </CardActions>
             </Card> 
               
           </Grid>
